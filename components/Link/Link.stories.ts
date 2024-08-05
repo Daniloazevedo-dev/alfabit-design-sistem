45ca82d1af3aef8d40f53b739d3d2113fd759a8b
Danilo Azevedo <danilo.ann@gmail.com>
2024-08-04 21:27:17 -0300

    Finalizando botão Link e iniciando Input

--- components/Link/Link.stories.ts
+++ components/Link/Link.stories.ts
@@ -5,8 +5,20 @@ import Link,  { LinkProps } from './Link'
 const meta: Meta<LinkProps> = {
     title: 'Molecules/Link',
     component: Link,
-    tags: ['autodocs'],
-    argTypes: {},
+    argTypes: {
+        children: {
+            type: 'string',
+        },
+        href: {
+            type: 'string'
+        },
+        disabled: {
+            type: 'boolean',
+        },
+        className: {
+            type: 'string',
+        },
+    },
 }
 
 export default meta
@@ -16,4 +28,20 @@ export const Primary: StoryObj<LinkProps> = {
         children: 'Link',
         href: '/',
     },
+}
+
+export const Disabled: StoryObj<LinkProps> = {
+    args: {
+        children: 'Link',
+        href: '/',
+        disabled: true,
+    }
+}
+
+export const Violet: StoryObj<LinkProps> = {
+    args: {
+        children: 'Link',
+        href: '/',
+        className: 'theme-violet',
+    }
 }
