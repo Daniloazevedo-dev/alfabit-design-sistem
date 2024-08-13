6def296c90104d4ab8b44fd4863e3183f1e25e6e
Danilo Azevedo <danilo.ann@gmail.com>
2024-08-12 21:02:12 -0300

    Criado Storybook Typography

---
+++ components/Typography/Typography.tsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import { Primary } from '../Button/Button.stories';
+
+export type TypographyProps = {
+    variant?: 'primary' | 'secondary' | 'tertiary';
+    size?: 'xs' | 'sm' | 'md' | 'x1' | 'title1' | 'title2' | 'title3';
+    children: React.ReactNode,
+    element?: keyof JSX.IntrinsicElements
+} & JSX.IntrinsicElements['p'];
+
+const variantClassMap = {
+    variant: {
+        primary: 'text-gray-primary',
+        secondary: 'text-gray-secondary',
+        tertiary: 'text-gray-tertiary',
+    },
+    size: {
+        xs: 'text-xs',
+        sm: 'text-sm',
+        md: 'text-md',
+        x1: 'text-x1',
+        title1: 'text-txl',
+        title2: 'text-tlg',
+        title3: 'text-tmd',
+    }
+}
+
+const Typography = ({ 
+    variant='primary', 
+    size='md', 
+    children, 
+    element='p',
+    ...rest 
+}: TypographyProps) => {
+    const Element = element as any;
+    return <Element className={`text-gray-primary text-sm font-normal 
+        ${ variantClassMap.variant[ variant ] } ${ variantClassMap.size[ size ] }`} { ...rest }>{ children }</Element>;
+    
+};
+
+export default Typography;
